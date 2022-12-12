ace9e8f46fd82680a3c0d38b14dc517cc274420f
idanbaril1 <117566999+idanbaril1@users.noreply.github.com>
2022-12-12 13:25:51 +0200

    ex3 after first meeting

---
+++ ex3/src/AST/AST_EXP_BINOP.java
@@ -0,0 +1,114 @@
+package AST;
+
+import TYPES.*;
+
+public class AST_EXP_BINOP extends AST_EXP
+{
+	int OP;
+	public AST_EXP left;
+	public AST_EXP right;
+	
+	/******************/
+	/* CONSTRUCTOR(S) */
+	/******************/
+	public AST_EXP_BINOP(AST_EXP left,AST_EXP right,int OP)
+	{
+		/******************************/
+		/* SET A UNIQUE SERIAL NUMBER */
+		/******************************/
+		SerialNumber = AST_Node_Serial_Number.getFresh();
+
+		/***************************************/
+		/* PRINT CORRESPONDING DERIVATION RULE */
+		/***************************************/
+		System.out.print("====================== exp -> exp BINOP exp\n");
+
+		/*******************************/
+		/* COPY INPUT DATA NENBERS ... */
+		/*******************************/
+		this.left = left;
+		this.right = right;
+		this.OP = OP;
+	}
+	
+	/*************************************************/
+	/* The printing message for a binop exp AST node */
+	/*************************************************/
+	public void PrintMe()
+	{
+		String sOP="";
+		
+		/*********************************/
+		/* CONVERT OP to a printable sOP */
+		/*********************************/
+		if (OP == 0) {sOP = "+";}
+		if (OP == 1) {sOP = "-";}
+		if (OP == 2) {sOP = "*";}
+		if (OP == 3) {sOP = "/";}
+		if (OP == 4) {sOP = "<";}
+		if (OP == 5) {sOP = ">";}
+		if (OP == 6) {sOP = "=";}
+
+		/*************************************/
+		/* AST NODE TYPE = AST SUBSCRIPT VAR */
+		/*************************************/
+		System.out.print("AST NODE BINOP EXP\n");
+		System.out.format("BINOP EXP(%s)\n",sOP);
+
+		/**************************************/
+		/* RECURSIVELY PRINT left + right ... */
+		/**************************************/
+		if (left != null) left.PrintMe();
+		if (right != null) right.PrintMe();
+
+		/***************************************/
+		/* PRINT Node to AST GRAPHVIZ DOT file */
+		/***************************************/
+		AST_GRAPHVIZ.getInstance().logNode(
+			SerialNumber,
+			String.format("BINOP(%s)",sOP));
+		
+		/****************************************/
+		/* PRINT Edges to AST GRAPHVIZ DOT file */
+		/****************************************/
+		if (left  != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,left.SerialNumber);
+		if (right != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,right.SerialNumber);
+	}
+	public TYPE SemantMe()
+	{
+		TYPE t1 = null;
+		TYPE t2 = null;
+		
+		if (left  != null) t1 = left.SemantMe();
+		if (right != null) t2 = right.SemantMe();
+		
+		if(OP==6){
+			if(t1 == t2){
+				return TYPE_INT.getInstance();
+			}
+			if(t1.isClass() && t2.isClass()){
+				TYPE_CLASS type1 = t1.father;
+				TYPE_CLASS type2 = t2.father;
+				if(type1.father == type2 || type2.father == type1){
+					return TYPE_INT.getInstance();
+				}
+				System.out.format(">> ERROR [%d:%d] cannot make binop between %s and %s\n",t1,t2);	
+				System.exit(0);
+				return null;
+			}
+			if((t1.isClass() || t1.isArray()) && t2)
+		}
+		
+		if ((t1 == TYPE_INT.getInstance()) && (t2 == TYPE_INT.getInstance()))
+		{
+			return TYPE_INT.getInstance();
+		}
+		if((OP==0) && (t1 == TYPE_STRING.getInstance()) && (t2 == TYPE_STRING.getInstance())){
+			return TYPE_STRING.getInstance();
+		}
+		System.out.format(">> ERROR [%d:%d] cannot make binop between %s and %s\n",t1,t2);	
+		System.exit(0);
+		return null;
+	}
+
+}
